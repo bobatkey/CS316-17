5ee9eab60cf3d8f539582015be69fe1854735475
Robert Atkey <bob.atkey@gmail.com>
2017-09-22 11:54:59 +0100

    End-of-lecture version of Lec03

--- lectures/Lec03.hs
+++ lectures/Lec03.hs
@@ -21,22 +21,24 @@ data MyType = Foo | Bar MyType deriving Show
 --   without parameters
 
 myVal :: MyType
-myVal = undefined
+myVal = Foo
 
 --   with parameters
 
 baz :: MyType -> MyType
-baz x = undefined
+baz x = Bar (Bar x)
 
 
 
 --   using conditional expression (e.g., not)
 
 not0 :: Bool -> Bool
-not0 x = undefined
+not0 x = if x then False else True  {-  x ? false : true -}
 
 gcd0 :: Int -> Int -> Int
-gcd0 = undefined -- with conditionals
+gcd0 x y = if x == y then x
+           else if x < y then gcd0 x (y - x)
+                else gcd0 (x - y) y
 
 
 
@@ -46,10 +48,13 @@ gcd0 = undefined -- with conditionals
 --   e.g., greatest common divisor (gcd is already a thing, so rename)
 
 gcd1 :: Int -> Int -> Int
-gcd1 = undefined
+gcd1 x y | x == y = x
+gcd1 x y | x < y  = gcd1 x (y - x)
+gcd1 x y | otherwise = gcd1 (x - y) y
 
 not1 :: Bool -> Bool
-not1 = undefined
+not1 x | x = False
+not1 _     = True
 
 
 
@@ -59,12 +64,14 @@ not1 = undefined
 --   e.g., not not again? (underscore patterns)
 
 not2 :: Bool -> Bool
-not2 = undefined
+not2 True  = False
+not2 False = True
 
 --   e.g., maybeApply, maybe?  (:info Maybe)
 
 maybeApply :: Maybe (a -> b) -> Maybe a -> Maybe b
-maybeApply = undefined
+maybeApply (Just f) (Just a) = Just (f a)
+maybeApply _        _        = Nothing
 
 {- Fred: -}
 
@@ -78,25 +85,47 @@ maybeApply = undefined
 --   do (_:_)
 
 -- append [1,2,3] [4,5,6] = [1,2,3,4,5,6]
--- append []       ys = ys
--- append (x : xs) ys = x : append xs ys
+
+-- append [] [4,5,6] = [4,5,6]
+
+-- append [1,2,3] [4,5,6] = [1,2,3,4,5,6] = [1, "append [2,3] [4,5,6]"] 
 
 append :: [x] -> [x] -> [x]
-append = undefined
+append []     ys = ys
+append (x:xs)   ys      =   x : (append xs ys)
+--     [1,2,3]  [4,5,6]     1   [2,3,4,5,6]
 
--- rev []
+-- rev [] 
 -- rev (_:_)
 
+-- rev [4,5,6] = [6,5,4] 
+-- rev [1,2,3,4] = [4,3,2,1]
+
+
 rev :: [x] -> [x]
-rev = undefined
+rev [] = []
+rev (x:xs) = append (rev xs) [x]
+
 
 -- sawPrefix [1,2,3] [1,2,3,4,5,6] = [4,5,6]
 -- sawPrefix [1,2,3] [1,2]         = ?
 
-sawPrefixOff :: Eq a => [a] -> [a] -> [a]
-sawPrefixOff = undefined
+-- 
+
+sawPrefixOff :: Eq a => [a] -> [a] -> Maybe [a]
+sawPrefixOff []     ys     = Just ys
+sawPrefixOff (x:xs) []     = Nothing
+sawPrefixOff (x:xs) (y:ys) = if x == y then sawPrefixOff xs ys else Nothing
+
+-- basename ".txt" "mylifestory.txt" == Just "mylifestory"
+
+basename :: String -> String -> Maybe String
+basename ext filename = case sawPrefixOff (rev ext) (rev filename) of
+                          Nothing -> Nothing
+                          Just str -> Just (rev str)
 
 -- (hint: need a Maybe!)
 
 hd :: [a] -> Maybe a
-hd = undefined
+hd []     = Nothing
+hd (x:xs) = Just x
