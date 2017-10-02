200690e0aaae84c47976c40cb1c8b7ef356f317a
Fredrik Nordvall Forsberg <fredrik.nordvall-forsberg@strath.ac.uk>
2017-10-02 13:48:56 +0100

    script for adding gitlab remote

---
+++ set-up-gitlab.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Add gitlab remote
+git remote add gitlab https://$USER@gitlab.cis.strath.ac.uk/cs316-2017/assessment-$USER.git && echo "Gitlab remote added successfully.\n"
+
+# Populate gitlab
+git push gitlab || exit 1
+
+# Instructions
+echo "To get the latest changes from github, just execute\n\n    git pull\n\non the command line. To push your changes to gitlab (e.g. for assessment), do \n\n    git push gitlab\n\non the command line. Enjoy!"
