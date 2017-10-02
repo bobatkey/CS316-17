7fd3f28b469bb012375e105935a4252811485ade
Fredrik Nordvall Forsberg <fredrik.nordvall-forsberg@strath.ac.uk>
2017-10-02 14:01:33 +0100

    updated instructions

--- set-up-gitlab.sh
+++ set-up-gitlab.sh
@@ -7,4 +7,4 @@ git remote add gitlab https://$USER@gitlab.cis.strath.ac.uk/cs316-2017/assessmen
 git push gitlab || exit 1
 
 # Instructions
-echo "\n\nAll set! To get the latest changes from github, just execute\n\n    git pull\n\non the command line. To push your changes to gitlab (e.g. for assessment), do \n\n    git push gitlab\n\non the command line. Enjoy!"
+echo "\n\nAll set! To get the latest changes from github, just execute\n\n    git pull\n\non the command line. To push your changes to gitlab (e.g. for assessment),\nfirst commit them\n\n    git commit -a -m \"<commit message>\"\n\nthen do \n\n    git push gitlab\n\non the command line. Enjoy!"
