435e8609a7766dcdde93da099f0b0149217e4b8a
Bob Atkey <bob.atkey@gmail.com>
2017-10-08 15:37:52 +0100

    Start Part II of Lecture 06 notes.

--- lectures/Lec06.hs
+++ lectures/Lec06.hs
@@ -102,40 +102,113 @@ fst2 = \(a,b) -> a
    that take other functions as input. -}
 
 
-{-    PART II : FUNCTIONS THAT TAKE FUNCTIONS AS INPUT -}
+{-     PART II : FUNCTIONS THAT TAKE FUNCTIONS AS INPUT
 
+   As I said in the introduction, Haskell treats functions as it does
+   any other kind of value. The can be returned by functions, as we
+   saw in Part I. We'll now look at how and why Haskell functions can
+   take functions as arguments.
 
+   Let's start by looking at a simple definition. Here is a definition
+   of the number 'ten' by adding '5' to itself: -}
 
 ten :: Int
 ten = add 5 5
 
+{- We now think to ourselves "there's nothing special about the number
+   '5' here, we could be adding any number to itself". So we move
+   from the specific '5' to a general 'x', which we make an argument
+   of the function. We now have a function that takes an 'Int' and
+   returns an 'Int': -}
+
 double :: Int -> Int
 double x = add x x
+
+{- Continuing this line of thought, we think to ourselves "there's
+   nothing special about 'add'ing here, we could use any operation
+   that takes two 'Int's and returns an 'Int'". So we move from the
+   specific 'add' to a general 'f', which we make an argument of the
+   function. We adjust the type again: 'add' has type 'Int -> Int ->
+   Int', so our new function takes a value of this type and returns a
+   function that takes 'Int's and returns 'Int's: -}
 
 applyCopy :: (Int -> Int -> Int) -> Int -> Int
 applyCopy f x = f x x
 
+{- 'applyCopy' is now a generally applicable function that takes /any/
+   two argument function on 'Int's, and /any/ 'Int' and passes that
+   'Int' twice to the given function.
+
+   We call 'applyOrder' a *higher order* function because it takes a
+   function as its argument. The order of a function refers to how
+   'functiony' its arguments are: A value with no arguments is of
+   order 0, a function with arguments that have no arguments is order
+   1, a function with arguments that take arguments is order 2, and so
+   on.
+
+   Because we have constructed 'applyCopy' by repeated moves from the
+   specific to the general, we can get back down to earth again by
+   applying 'applyCopy' to the original specific 'add' and '5'. So we
+   can recover the 'double' function by applying 'applyCopy' to 'add': -}
+
 double2 :: Int -> Int
 double2 = applyCopy add
+
+{- And we can recover 'ten' by applying 'double2' to '5': -}
 
 ten2 :: Int
 ten2 = double2 5
 
+{- When we moved from 'ten' to 'applyCopy' above, we didn't change the
+   types much: in the end, 'applyCopy' still worked on 'Int's. In the
+   example, we will see how moving from specific functions to more
+   general ones allows us to also make the types more general too.
+
+   The 'quadruple' function applies 'double' twice to double a number: -}
 
 quadruple :: Int -> Int
 quadruple x = double (double x)
 
+{- As above, there is nothing special about 'double' here. We move from
+   the specific 'double' to a general 'f' to make the 'twice'
+   function, which applies a function to an argument, and then applies
+   it again. Only this time, we also make the type more general --
+   there is nothing specific to 'Int's in the definition of 'twice' so
+   we can replace the specific 'Int' by the general 'a': -}
+
 twice :: (a -> a) -> a -> a
 twice f x = f (f x)
 
-twice2 :: (a -> a) -> a -> a
-twice2 f = \x -> f (f x)
+{-    EXERCISE: what is the more general type for 'applyCopy' above?
+
+   We needn't always work out the more general type for ourselves. We
+   can ask GHCi to do it for us:
+
+      Prelude> :t twice
+      (t -> t) -> t -> t
+
+   As with 'applyCopy' above, we can recover 'quadruple' by applying
+   twice to 'double', or any other way of writing 'double' that we can
+   think of. This is where the anonymous '\'/lambda notation comes in
+   very useful for writing short functions that are only mentioned
+   once without needing to think of a name. We can write 'double' as
+   '\x -> x + x', which in some contexts may be clearer than the word
+   'double'. -}
 
 quadruple2 :: Int -> Int
 quadruple2 = twice (applyCopy add)
       --     twice double
       --     twice (\x -> x + x)
       --     twice (\x -> 2 * x)
+
+{- Because 'twice' is more general than 'quadruple', we can use it again
+   for new purposes. For example, octupling:-}
+
+octtuple :: Int -> Int
+octtuple = twice quadruple
+
+{- FIXME: another example of twice. -}
+
 
 doubleAll :: [Int] -> [Int]
 doubleAll [] = []
