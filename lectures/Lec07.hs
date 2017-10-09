7389293981914b91ba173736d7836d6956d5079f
Robert Atkey <bob.atkey@gmail.com>
2017-10-09 10:23:07 +0100

    Lecture 07 pre-lecture template

---
+++ lectures/Lec07.hs
@@ -0,0 +1,20 @@
+module Lec07 where
+
+import Prelude hiding
+  (Maybe (..), Bool (..), String)
+
+{-    LECTURE 07 : DECLARING TYPES AND CLASSES
+
+-}
+
+{-    PART I : TYPE SYNONYMS -}
+
+
+
+{-    PART II : DATA TYPES -}
+
+
+
+{-    PART III : TYPE CLASSES -}
+
+
