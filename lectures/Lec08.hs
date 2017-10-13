9d42ea40f651bb33892918c6e95acc46968eb2db
Robert Atkey <bob.atkey@gmail.com>
2017-10-13 10:47:20 +0100

    lecture 08 prep

---
+++ lectures/Lec08.hs
@@ -0,0 +1,13 @@
+module Lec08 where
+
+import Test.QuickCheck
+
+{-   LECTURE 08: QUICKCHECK -}
+
+{-   PART I : WRITING INDIVIDUAL TEST CASES -}
+
+
+{-   PART II : PROPERTY BASED TESTING WITH QUICKCHECK -}
+
+-- Why not test with lots of examples, not just one?
+
