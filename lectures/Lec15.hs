28047864798b1ed37194a5902afdcdb0256086cc
Bob Atkey <bob.atkey@gmail.com>
2017-11-21 20:35:16 +0000

    More notes for Lecture 15, not finished yet

--- lectures/Lec15.hs
+++ lectures/Lec15.hs
@@ -4,21 +4,56 @@ import Control.Applicative hiding (some, many)
 import Control.Monad
 import Data.Char
 
-{- LECTURE 15 : MORE MONADIC PARSING -}
+{-     LECTURE 15 : MORE MONADIC PARSING -}
 
-{- Parsers of things are functions from strings to the possibility of
+{- First we'll recap the 'Parser' type we defined in the last lecture.
+
+   Parsers of things are functions from strings to the possibility of
    pairs of things and strings: -}
 
 newtype Parser a = MkParser (String -> Maybe (a,String))
 
+{- We apply a 'Parser' to a string using 'runParser', which returns
+   either 'Nothing' if the parser fails to extract anything from the
+   string, or 'Just (a, s)' if it extracted 'a' with leftover string
+   's'. We'll see more examples of this below. -}
 
 runParser :: Parser a -> String -> Maybe (a, String)
 runParser (MkParser p) input = p input
 
+{- What makes 'Parser's so useful is that they support lots of
+   structure: 'Functor', 'Applicative', 'Alternative', and 'Monad'. We
+   introduced this structure in the previous lecture, so we briefly
+   reintroduce it here.
+
+   The 'Functor' typeclass includes the 'fmap' function, which allows
+   us to post-process the results of a 'Parser', as we saw last time. -}
+
 instance Functor Parser where
+  -- fmap :: (a -> b) -> Parser a -> Parser b
   fmap f (MkParser p) =
     MkParser (\input -> fmap (\(a,rest) -> (f a,rest)) (p input))
 
+{- The 'Alternative' instance for 'Parser's allows us to try one parser
+   and then another if the first one fails ('p1 <|> p2'), or to write
+   a parser that always fails ('empty'). -}
+
+orElse :: Parser a -> Parser a -> Parser a
+orElse (MkParser p1) (MkParser p2) =
+  MkParser (\input -> case p1 input of
+                        Nothing -> p2 input
+                        Just (a,s) -> Just (a,s))
+
+failure :: Parser a
+failure = MkParser (\input -> Nothing)
+
+instance Alternative Parser where
+  empty = failure
+  (<|>) = orElse
+
+{- The 'Applicative' instance for 'Parser's allows us to run one parser
+   and then another one afterwards. The leftover input from the first
+   parser is fed into the second one. -}
 
 andThen :: Parser a -> Parser b -> Parser (a,b)
 andThen (MkParser p1) (MkParser p2) =
@@ -39,18 +74,10 @@ instance Applicative Parser where
   -- (<*>) :: Parser (a -> b) -> Parser a -> Parser b
   pf <*> pa = (\(f, a) -> f a) <$> (pf `andThen` pa)
 
-orElse :: Parser a -> Parser a -> Parser a
-orElse (MkParser p1) (MkParser p2) =
-  MkParser (\input -> case p1 input of
-                        Nothing -> p2 input
-                        Just (a,s) -> Just (a,s))
-
-failure :: Parser a
-failure = MkParser (\input -> Nothing)
-
-instance Alternative Parser where
-  empty = failure
-  (<|>) = orElse
+{- The 'Monad' instance allows us to run a 'Parser' and then choose
+   another 'Parser' based on the data parsed by the first. This is
+   useful for making decisions based on the input, such as filtering
+   out certain things, as we saw in the previous lecture. -}
 
 instance Monad Parser where
   MkParser p >>= f =
@@ -68,6 +95,8 @@ char = MkParser (\input -> case input of
                              ""   -> Nothing
                              c:cs -> Just (c,cs))
 
+{- Finally, we have a 'Parser' that only succeeds when we are at the end
+   of input (eoi). -}
 eoi :: Parser ()
 eoi = MkParser (\input -> case input of
                             ""  -> Just ((), "")
@@ -107,41 +136,79 @@ satisfies p = do
   c <- char
   if p c then return c else empty
 
-leftParen :: Parser ()
-leftParen = ignore (satisfies (\x -> x == '('))
-
-leftSqBracket :: Parser ()
-leftSqBracket = ignore (satisfies (\x -> x == '['))
+{- We'll also use the function 'ignore', which uses 'fmap' to
+   post-process the result of a parser than returns an 'a' to throw it
+   away and just return '()' on success. -}
 
 ignore :: Parser a -> Parser ()
 ignore p = fmap (\_ -> ()) p
 
+{- Using 'satisfies' and 'ignore', we can write a parser than recognises
+   left parentheses '(': -}
 
--- Whitespace
+leftParen :: Parser ()
+leftParen = ignore (satisfies (\x -> x == '('))
+
+{- Let's see it working:
+
+       λ> runParser leftParen "("
+       Just ((),"")
+       λ> runParser leftParen ")"
+       Nothing
+       λ> runParser leftParen "(abc"
+       Just ((),"abc")
+
+   In the same way, we can write a parser that only recognises left
+   square brackets '[': -}
+
+leftSqBracket :: Parser ()
+leftSqBracket = ignore (satisfies (\x -> x == '['))
+
+{- Using the same pattern, we can write many useful parsers. Here's one
+   that recognises whitespace: spaces, newlines, tabs, and carriage
+   returns: -}
+
 space :: Parser ()
 space = ignore (satisfies (\x -> x == ' ' || x == '\n' || x == '\t' || x == '\r'))
 
--- alternatively:
---  space = ignore (satisfies isSpace)
+{- Alternatively, we can use the built-in function 'isSpace' from the 'Data.Char' module (imported above):
 
--- Literal characters
+     space = ignore (satisfies isSpace)
+
+   Another useful little parser is the one that is given a specific
+   character 'c', and succeeds only if the first character in the
+   input is 'c': -}
+
 isChar :: Char -> Parser ()
 isChar c = ignore (satisfies (\x -> x == c))
 
--- literal strings
-----  runParser (string "hello") "hello!!!" = Just ((), "!!!")
+{- Using 'isChar', we can use it repeatedly to build a parser that
+   recognises a given string: -}
 
 string :: String -> Parser ()
 string []     = nothing -- pure ()
 string (e:es) = (\() () -> ()) <$> isChar e <*> string es
 
+{- For example:
+
+       λ> runParser (string "hello") "hello!!!"
+       Just ((),"!!!")
+
+   The function 'string' parses more than one character by recursing
+   over the sequence of characters it has been told to look for. But
+   what if we don't know what we are looking for in advance. How can
+   we run a parser repeatedly until it fails? -}
 
 
+{-     PART II. Repeated Parsing
+
+   
+-}
 
 
 -- zero or more
 many :: Parser a -> Parser [a]
-many p = (\ a as -> a:as) <$> p <*> many p 
+many p =  (\a as -> a:as) <$> p <*> many p 
       <|> pure []
 
 -- fmap f s = pure f <*> s
@@ -149,14 +216,16 @@ many p = (\ a as -> a:as) <$> p <*> many p
 
 {- Examples using 'many':
 
-     λ> runParser (many space) ""
-     Just ([], "")
-     λ> runParser (many space) "    "
-     Just ([(),(),(),()], "")
-     λ> runParser (many char) "    "
-     ["    "]
-     λ> runParser (many char) " sdhgsjfhksdh   "
-     [" sdhgsjfhksdh   "]
+      λ> runParser (many space) ""
+      Just ([], "")
+      λ> runParser (many space) "    "
+      Just ([(),(),(),()], "")
+      λ> runParser (many char) "    "
+      Just ("    ","")
+      λ> runParser (many char) " sdhgsjfhksdh   "
+      Just (" sdhgsjfhksdh   ","")
+
+   
 -}
 
 -- one or more
@@ -166,18 +235,23 @@ some p = (\ a as -> a:as) <$> p <*> many p
 {- Examples using 'some'
 
      λ> runParser (some space) ""
-     []
+     Nothing
      λ> runParser (some space) " "
-     [[()]]
+     Just ([()], "")
 -}
 
 
--- things separated by other things
+{- Things separated by other things -}
+
 sepBy :: Parser () -> Parser a -> Parser [a]
 sepBy separator thing =
       (:) <$> thing
           <*> many ((\ () t -> t) <$> separator <*> thing)
   <|> pure []
+
+
+
+{- Extended example: Writing a CSV parser -}
 
 field :: Parser String
 field =  (\_ s _ -> s) <$> isChar '\"'
@@ -199,11 +273,6 @@ field2 =  (\_ s _ -> s) <$> isChar '\"'
                         <*> isChar '\"'
       <|> many (satisfies (\x -> x /= ':' && x /= '\n'))
 
-{-
-field' = MkParser myFieldParser
-  where myFieldParser input =
-          _
--}
   -- 1  ,2 ,3
   -- a,b,c
 
